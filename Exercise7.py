78e58c27f97ce5749d194436ac3b8679f1259479
MchllD <mich11dimafelix@gmail.com>
2024-01-26 11:29:23 +0800

    Function to count occurrences

---
+++ Exercise7.py
@@ -0,0 +1,29 @@
+# Exercise 7: Return the count of a given substring from a string
+# Write a program to find how many times substring “Emma” appears in the given string.
+
+# pseudocode
+
+
+# Function to count occurrences of a substring in a given string
+# Iterate over the indices of the statement
+# Check if the substring matches the segment of the statement
+# Return the count of occurrences
+# Count occurrences of 'Emma'
+# Count occurrences of 'emma' (case-insensitive)
+# Combine the counts of 'Emma' and 'emma'
+
+# *************************************** actual code *****************************************
+
+
+# Function to count occurrences of a substring in a given string
+def count_substring(statement, substring):
+    print(f"Given String: {statement}")
+    count = 0
+
+
+# Iterate over the indices of the statement
+# Check if the substring matches the segment of the statement
+# Return the count of occurrences
+# Count occurrences of 'Emma'
+# Count occurrences of 'emma' (case-insensitive)
+# Combine the counts of 'Emma' and 'emma'
